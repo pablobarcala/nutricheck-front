e1544b4bce124d9a26b3af2ae09adf288a3c5cd4
pablobarcala <pablobarcala4@gmail.com>
2025-06-28 14:26:05 -0300

    agregar api remota

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,5 @@
 export const environment = { 
     // API :"http://localhost:5070"
-    API: "https://localhost:7147" 
+    // API: "https://localhost:7147" 
+    API: "https://nutricheck-back.onrender.com"
 };
