09a9fd24ddf8a2390a371b6dd5ed177ef6ff204f
BautistaConta <bautistaconta0@gmail.com>
2025-07-09 11:25:15 -0300

    Arreglos responsive

--- src/app/nutricionista/pacientes/page.tsx
+++ src/app/nutricionista/pacientes/page.tsx
@@ -31,39 +31,39 @@ export default function MisPacientesPage() {
   }, []);
 
   return (
-    <div className="py-10 w-full font-[Montserrat] relative">
-      <h1 className="text-3xl font-bold mb-4">Pacientes</h1>
+   <div className="py-10 w-full font-[Montserrat] relative">
+  <h1 className="text-3xl font-bold mb-4">Pacientes</h1>
 
-      <div className="flex gap-4 flex-wrap justify-start items-center">
-        {pacientes.map((paciente: any) => (
-          <button
-            key={paciente.id}
-            onClick={() => router.push(`/nutricionista/pacientes/${paciente.id}`)}
-            className="cursor-pointer bg-neutral-100 dark:bg-neutral-100/10 font-bold text-xl flex flex-col gap-2 text-left py-2 pl-2 pr-24 border border-neutral-200 dark:border-neutral-200/10 rounded-md hover:bg-black/5 dark:hover:bg-white/5 transition"
-          >
-            {paciente.nombre}
-            <span className="text-sm font-normal">{paciente.email}</span>
-          </button>
-        ))}
-      </div>
-
+  <div className="flex gap-4 flex-wrap justify-start items-center">
+    {pacientes.map((paciente: any) => (
       <button
-        onClick={() => setMostrarModal(true)}
-        className="fixed bottom-6 right-6 text-xl bg-green-500 text-black rounded-full w-12 h-12 flex items-center justify-center hover:bg-green-600 z-50"
-        title="Vincular nuevo paciente"
+        key={paciente.id}
+        onClick={() => router.push(`/nutricionista/pacientes/${paciente.id}`)}
+        className="cursor-pointer w-full sm:w-[280px] bg-neutral-100 dark:bg-neutral-100/10 font-bold text-lg flex flex-col gap-1 text-left py-3 px-4 border border-neutral-200 dark:border-neutral-200/10 rounded-md hover:bg-black/5 dark:hover:bg-white/5 transition"
       >
-        +
+        <span>{paciente.nombre}</span>
+        <span className="text-sm font-normal truncate">{paciente.email}</span>
       </button>
+    ))}
+  </div>
 
-      {mostrarModal && (
-        <ModalBuscarPaciente
-          onClose={() => setMostrarModal(false)}
-          onPacienteVinculado={() => {
-            fetchPacientes(); // 🔁 Recarga pacientes al vincular
-            setMostrarModal(false); // Cierra el modal
-          }}
-        />
-      )}
-    </div>
+  <button
+    onClick={() => setMostrarModal(true)}
+    className="fixed bottom-6 right-6 text-xl bg-green-500 text-black rounded-full w-12 h-12 flex items-center justify-center hover:bg-green-600 z-50"
+    title="Vincular nuevo paciente"
+  >
+    +
+  </button>
+
+  {mostrarModal && (
+    <ModalBuscarPaciente
+      onClose={() => setMostrarModal(false)}
+      onPacienteVinculado={() => {
+        fetchPacientes();
+        setMostrarModal(false);
+      }}
+    />
+  )}
+</div>
   );
 }
