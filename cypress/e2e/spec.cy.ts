1cc2493d04bbc1609acbbfa5afb097c717de2832
oskilord <aleepiccolo16@gmail.com>
2025-07-12 13:06:55 -0300

    iniciode cypress parte 1 -landingpage.

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,21 +1,18 @@
-describe('Flujo completo: Login, buscar paciente, vincular y logout', () => {
+describe('Flujo completo', () => {
   it('Realiza el flujo completo correctamente', () => {
-    cy.visit('http://localhost:3000/login');
-    
-    cy.get('#email').type('pablo2@correo.com');
-    cy.get('#password').type('12345678');
-    cy.get('.from-\\[\\#4AFF50\\]').click();
-    cy.get('body').click();
-    cy.get(':nth-child(3) > .flex').click();
-    cy.get('.fixed').click();
-    cy.get('.bg-neutral-900 > .flex > .p-2').type('Test Paciente');
-    cy.get('.bg-\\[\\#4AFF50\\]').click();
-    cy.get('button.text-white.rounded-md.font-bold').click();
-    cy.on('window:alert', (txt) => {
-      expect(txt).to.contains('Paciente vinculado correctamente');
-    });
-    
-    cy.get('.flex > .bg-green-500').click();
-    cy.get('.space-x-4 > .cursor-pointer').click();
+    cy.visit('http://localhost:3000/');
+    //verifica el logo
+    cy.get('.ml-3').should('be.visible');
+      //verifica el boton caracteristicas
+    cy.get('[href="#features"]').should('exist').click();
+    //verifica el boton como funciona
+    cy.get('[href="#how-it-works"]').should("exist").click();
+    //verifica boton testimonios
+    cy.get('[href="#testimonials"]').should("exist").click();
+    //verifica boton contacto
+    cy.get('[href="#testimonials"]').should("exist").click();
+   
+
+   
   });
 });
