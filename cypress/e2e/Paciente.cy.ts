5f3b1681d78efc41b5959e42bc30bf416027271e
oskilord <aleepiccolo16@gmail.com>
2025-07-29 16:39:38 -0300

    actualizacion cypres

--- cypress/e2e/Paciente.cy.ts
+++ cypress/e2e/Paciente.cy.ts
@@ -1,32 +1,24 @@
-describe('Flujo completo', () => {
+describe('Flujo de Paciente Logeado', () => {
   it('Realiza el flujo completo correctamente', () => {
     cy.visit('https://www.nutricheck.me/');
-    //verifica el logo
-    cy.get('.ml-3').should('be.visible');
-      //verifica el boton caracteristicas
-    cy.get('[href="#features"]').should('exist').click();
-    //verifica el boton como funciona
-    cy.get('[href="#how-it-works"]').should("exist").click();
-    //verifica boton testimonios
-    cy.get('[href="#testimonials"]').should("exist").click();
-    //verifica boton contacto
-    cy.get('[href="#testimonials"]').should("exist").click();
-   
-    // Ahora paso para el registro del paciente 
-    cy.get('.space-x-4 > .bg-green-600').should("exist").click();
-    
-    //aqui se rellena el formulario para luego crear la cuenta del paciente.
-
-    cy.get('#nombre').type("Pablo");
+    cy.get('.cursor-pointer').should('exist').click();
     cy.get('#email').type("pablo4@correo.com");
     cy.get('#password').type("12345678");
-   cy.get('#confirmPassword').type("12345678");
-   cy.get('.space-y-6 > .bg-gradient-to-r').click();
-   cy.get(':nth-child(1) > .p-2').type("85");
-   cy.get(':nth-child(2) > .p-2').type("180");
-   cy.get(':nth-child(3) > .p-2').type('2000-07-13');
-   cy.get(':nth-child(4) > .p-2').select('masculino');
-   cy.get(':nth-child(5) > .p-2').select('Sedentario - sin ejercicio');
+   cy.get('.from-\\[\\#4AFF50\\]').click();
+   cy.get('.bg-green-800').click();
+   cy.get('.gap-2 > :nth-child(4)').click();
+  cy.get('.max-h-60 > :nth-child(1) > .font-bold').click();
+   cy.contains('button', 'Desayuno').click();
+   cy.get('.max-h-60 > :nth-child(3)').click();
+   cy.get('.justify-end > .cursor-pointer').click();
+  cy.get('.space-x-4 > :nth-child(2) > .flex').click();
+  cy.get('.py-10 > :nth-child(2)').click();
+  cy.get('.mt-4').click();
+  cy.get('.py-10 > :nth-child(3)').click();
+  cy.get('.mt-4').click();
+  cy.get('.space-x-4 > :nth-child(4) > .flex').click();
+  cy.get('.space-x-4 > .cursor-pointer').click();
+  
    
    
   });
